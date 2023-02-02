91aec85bde2c34be00e623789fdd362b66c37884
garceteheber800gmail.com <garceteheber800gmail.com>
2023-02-02 12:36:46 -0300

    cambios

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -2,17 +2,62 @@
 A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
+a=
 */
+
 
 function Rectangulo () 
 {
+     /*A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
+     perimetro : largo *2  + base * 2
+     para 3 hilos de alambre tengo que hacer perimetro  * 3
+      */
+
+     let largo;
+     let ancho;
+     let perimetro;
+     let alambre;
+
+
+     largo = document.getElementById("txtIdLargo").value;
+     ancho = document.getElementById("txtIdAncho").value;
+
+     largo = parseFloat (largo);
+     ancho = parseFloat(ancho);
+
+    perimetro = largo * 2 + ancho * 2;
+    alambre = perimetro * 3; 
+    
+    console.log(alambre)
+
+
+
+
+
+
 
 }
 function Circulo () 
 {
-	
+	/* B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
+    perimetro de un circulo es  : 2 * 3,14 * R */
+
+    let radio;
+    let perimetro;
+    let alambre;
+
+  
+
+
+
+
 }
 function Materiales () 
 {
-	
+	 /*C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
+     para sacar el are de un rectangulo se hace : largo * ancho
+     para sacar las bolsas de cemento multiplico metro cuadrado por cemento y luego cal  */
+
+
+     
 }
--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -4,10 +4,40 @@ B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en
 */
 function FahrenheitCentigrados () 
 {
+    /* A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
+   como pasarla : (32 °F − 32) × 5/9 = 0 °C */
+   let far;
+   let centigrados;
+
+
+   far = document.getElementById("txtIdTemperatura").value;
+   far = parseFloat (far)
+
+   centigrados = (far - 32) * 5 /9
+
+  alert(far + " Farenheit es igual a " + centigrados)
 	
 }
 
 function CentigradosFahrenheit () 
 {
+   /* A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
+   como pasarla : (32 °F − 32) × 5/9 = 0 °C */
+   let far;
+   let centigrados;
+
+
+   far = document.getElementById("").value;
+   far = parseFloat (far)
+
+   centigrados = (far - 32) * 5 /9
+
+  alert(far + " Farenheit es igual a " + centigrados)
+
+   
+
+
+
+
 	
 }
