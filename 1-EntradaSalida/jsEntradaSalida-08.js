f387324bd91ed641a1e52fcf4f25f0f877c97c42
garceteheber800gmail.com <garceteheber800gmail.com>
2023-02-02 23:01:35 -0300

    iva

--- 1-EntradaSalida/jsEntradaSalida-08.js
+++ 1-EntradaSalida/jsEntradaSalida-08.js
@@ -2,10 +2,39 @@
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resto entre el dividendo y el divisor.
-ej.: "El resto es 0 ."*/
+ej.: "El resto es 0 .
+
+Ejercicio 8.bis: Ingresar dos numeros por id
+Se pide:
+mostrar por alert:
+a) La suma de los dos numeros
+b) El promedio de los numeros
+c) El resto de los numeros (el primero en modulo del segundo) 
+
 function SacarResto()
 {
-	let numDiviendo;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*let numDiviendo;
 	let numDivisor;
 	let mensaje
 	
@@ -17,7 +46,7 @@ function SacarResto()
 	numDivisor = parseInt (numDivisor)
 	mensaje	= "El resto es " + numDiviendo % numDivisor
 
-	alert ( mensaje)
+	alert ( mensaje) */
 
 	
 	
