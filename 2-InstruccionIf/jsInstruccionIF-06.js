9e4799310f6ba04437129f7d8e2c5754af69b8d8
garceteheber800gmail.com <garceteheber800gmail.com>
2023-02-08 09:01:57 -0300

    6

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,26 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad
+	let mensaje
 
+	edad = parseInt (document.getElementById("txtIdEdad").value); 
+
+	if (edad  >= 18)
+	{
+		mensaje = "Es mayor de edad "
+
+	}else if (edad  >= 13  ) 
+	{
+
+		mensaje= "Es es un adolecente"
+
+	}else
+	{
+
+		mensaje= "es un niño"
+
+	}	
+		alert(mensaje)
 
 
 }//FIN DE LA FUNCIÓN
