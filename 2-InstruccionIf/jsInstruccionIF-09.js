435ec4c5a8668e0a1f14e7d05a0886793fc99b1b
garceteheber800gmail.com <garceteheber800gmail.com>
2023-02-08 10:20:25 -0300

    cambios

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,12 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
+	let numero;
+	let max = 10;
+	let min = 1;
 
+	numero =Math.floor(Math.random() * max + min );
+
+	alert(numero);
+	
+	
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,18 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	let random;
+	let mensaje;
+	random =  Math.floor(Math.random() * 10) + 1;
 
+	if (random >= 9 ) 
+	{
+		 mensaje =  "Excelente" + random;
+	}else if (random >=  4 ) 
+	{ 
+		mensaje = "APROBO " + random;
+	}else {	
+		mensaje = "Vamos la proxima se puede  " + random
+	}
+
+	alert(mensaje)
 }//FIN DE LA FUNCIÓN
