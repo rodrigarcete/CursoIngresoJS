759047f472a6c16d8e0cfbc93e5506948267bf39
garceteheber800gmail.com <garceteheber800gmail.com>
2023-02-20 23:34:25 -0300

    while

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -3,8 +3,47 @@ al presionar el botón mostrar 10 repeticiones
 con números DESCENDENTES, desde el 10 al 1.*/
 function mostrar()
 {
+	let numeroIngresado;
+	let contador;
+	let contadorDeMayores;
 	
-	let	contador;
+
+	contador=0
+	contadorDeMayores= 0;
+	
+	while(contador< 5)
+	{
+		numeroIngresado= prompt("ingresa un numero");
+		numeroIngresado= parseInt(numeroIngresado);
+		while(isNaN(numeroIngresado))
+		{
+			numeroIngresado= prompt("error, ingresa un numero valido");
+			numeroIngresado= parseInt(numeroIngresado);
+		}
+		if(numeroIngresado > 9 && numeroIngresado < 21)
+		{
+			contadorDeMayores= contadorDeMayores + 1;	
+		}
+
+		contador= contador + 1; 
+	}
+
+	alert("la cantidad de numeros que se encuentran entre 10 y 20 (incluidos) es de: " + contadorDeMayores);
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*let	contador;
 
 	
 	contador = 1;
@@ -15,6 +54,6 @@ function mostrar()
 			contador = contador + 1;
 			alert(contador);
 	}
-	alert('iteración while');
+	alert('iteración while'); */
 
 }//FIN DE LA FUNCIÓN
