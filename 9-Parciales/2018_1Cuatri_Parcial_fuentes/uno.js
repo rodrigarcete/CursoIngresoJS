acad55cba5fd62999ec1df4acbea5a58e477d070
garceteheber800gmail.com <garceteheber800gmail.com>
2023-02-28 02:18:59 -0300

    parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -11,9 +11,39 @@ c) Cuántas unidades de jabones hay en total
 */
 function mostrar()
 {
-
-
-
+    let tipo;
+    let precio;
+    let cantidad;
+    let marca;
+    let fabricante;
+    let acumuladorBarbijo = 0;
+    let acumuladorAlcohol = 0;
+    let acumuladorJabon = 0;
+    let contBarbijo = 0;
+    let contAlcohol = 0;
+    let contJabon = 0;
+    let precioAlcoholBarato;
+    let cantidadAlcoholBarato;
+    let fabricanteAlcoholBarato;
+    let promedioCompra;
+    let mayorTipo;
+    //
+  /*  for(let i = 0; i < 5;i++)
+    {
+       tipo = prompt('Ingrese tipo de "barbijo" , "jabon"  o "alcohol":');
+       while (tipo != "barbijo" && tipo != "jabon" && tipo != "alcohol" ) 
+       { tipo = prompt ('Tipo invalido. Ingrese tipo de "barbijo" , "jabon"  o "alcohol": ');
+       }
+    
+       precio = parseFloat(prompt("Ingrese  precio $(100-300):"));
+       while( precio < 100  || precio > 300 || isNaN (precio)){  //!(precio >= 100 && precio <= 300)
+        parseInt(prompt("Error. Ingrese  precio $(100-300):"));  
+    
+       }*/
+    
+       precio = parseFloat(prompt("Ingrese  precio $(100-300):"));
+       while( precio < 100  || precio > 300 || isNaN (precio)){  //!(precio >= 100 && precio <= 300)
+        parseInt(prompt("Error. Ingrese  precio $(100-300):"));
 
     
 }
